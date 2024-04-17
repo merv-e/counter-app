9f8428260eae5e8c4805977800a12d285637a640
merv-e <34655454+merv-e@users.noreply.github.com>
2024-04-17 17:30:37 +0300

    minor change is made.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,28 @@
 import { useDispatch, useSelector } from "react-redux";
 import classes from "./Header.module.css";
-import {logout} from '../features/authenticationSlice/authSlice'
+import { logout } from "../features/authenticationSlice/authSlice";
 
 const Header = () => {
   const isUserLoggedIn = useSelector((state) => state.auth.auth);
-  console.log(isUserLoggedIn);
-
   const dispatch = useDispatch();
-
-  //LOGOUT
-
 
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
       <nav>
-        <ul>
-          <li>
-            <a href="/">My Products</a>
-          </li>
-          <li>
-            <a href="/">My Sales</a>
-          </li>
-          <li>
-            {/* onClick functionality can be added to login button as well, but there is no seperate page for that yet. */}
-            {isUserLoggedIn ? <button onClick={() => dispatch(logout())}>Logout</button> : <button>Login</button>}
-          </li>
-        </ul>
+        {isUserLoggedIn && (
+          <ul>
+            <li>
+              <a href="/">My Products</a>
+            </li>
+            <li>
+              <a href="/">My Sales</a>
+            </li>
+            <li>
+              <button onClick={() => dispatch(logout())}>Logout</button>
+            </li>
+          </ul>
+        )}
       </nav>
     </header>
   );
