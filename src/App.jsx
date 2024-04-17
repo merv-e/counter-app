34278138be7dea9efca3fe0f0c2c72ad85bfb301
merv-e <34655454+merv-e@users.noreply.github.com>
2024-04-17 16:35:17 +0300

    the conditional render logic is also implemented (after user is authenticated, the user profile page will be displayed.) In addition to that, for basic validation of the user, some user-friendly approaches are added while user is logging in.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import { Fragment } from "react";
+import { useSelector } from "react-redux";
+
 import Counter from "./components/Counter";
 import UserProfile from "./components/UserProfile";
+import Header from "./components/Header";
+import Auth from "./components/Auth";
 
 function App() {
+  const isUserAuthorized = useSelector((state) => state.auth.auth);
+
+  console.log(isUserAuthorized);
+
   return (
     <Fragment>
+      <Header />
+      {isUserAuthorized ? <UserProfile /> : <Auth />}
       {/* <Counter /> */}
-      <UserProfile />
     </Fragment>
   );
 }
--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,19 +1,62 @@
-import classes from './Auth.module.css';
+import { useState } from "react";
+import classes from "./Auth.module.css";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import {login} from '../features/authenticationSlice/authSlice'
 
 const Auth = () => {
+  const dispatch = useDispatch();
+
+  const [userEmail, setUserEmail] = useState("");
+  const [userPw, setUserPw] = useState("");
+
+  const handleLogin = () => {
+    dispatch(login())
+    console.log("Login is successful!");
+  };
+
+  //disable the login button under these circumstances: // In other names : Basic Validity
+
+  //NOTE : add regex ;)
+  const validEmail = userEmail.length > 0 && userEmail.includes("@");
+  const validPassword = userPw.length >= 8;
+  const [isBtnDisabled, setIsBtnDisabled] = useState(true);
+
+  useEffect(() => {
+    if (validEmail && validPassword) {
+      setIsBtnDisabled(false);
+    } else setIsBtnDisabled(true);
+  }, [validEmail, validPassword]); 
+
   return (
     <main className={classes.auth}>
       <section>
         <form>
           <div className={classes.control}>
-            <label htmlFor='email'>Email</label>
-            <input type='email' id='email' />
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              value={userEmail}
+              onChange={(e) => setUserEmail(e.target.value)}
+            />
           </div>
           <div className={classes.control}>
-            <label htmlFor='password'>Password</label>
-            <input type='password' id='password' />
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              value={userPw}
+              onChange={(e) => setUserPw(e.target.value)}
+            />
           </div>
-          <button>Login</button>
+          <button
+            onClick={handleLogin}
+            //  disabled={!validEmail && !validPassword}
+            className={isBtnDisabled ? "btn-disabled" : "btn"}
+          >
+            Login
+          </button>
         </form>
       </section>
     </main>
